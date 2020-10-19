a98f9732f765263f2dfee4e8ca2204c1a91da2af
HEATAO <heatao59@gmail.com>
2020-10-19 18:47:53 +0800

    "试验map的深拷贝"

---
+++ src/test/MapClone.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.HashMap;
+
+public class MapClone {
+    public static void main(String[] args) {
+        HashMap<Integer,Object> map = new HashMap<Integer,Object>();
+        map.clone();
+    }
+}
