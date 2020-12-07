ae59ce381fb190390c4a179148140c424a801ffc
HEATAO <heatao59@gmail.com>
2020-12-07 16:51:57 +0800

    50，快速幂

--- src/medium/MyPow50.java
+++ src/medium/MyPow50.java
@@ -9,6 +9,10 @@ import java.math.BigInteger;
  * 说明:
  * -100.0 < x < 100.0
  * n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+ *
+ * 这个题的正解还是快速幂算法，有两个角度理解的快读幂
+ * 一种是从分治的角度理解，用递归解，是否要多乘一个x，从右向左理解
+ * 一种是用二进制的角度理解，用迭代解
  */
 public class MyPow50 {
     /**
@@ -32,6 +36,28 @@ public class MyPow50 {
             if (ans == 0) return 0;
         }
         return ans;
+    }
+
+    public double quickMul(double x, long N) {
+        if (N == 0)
+            return 1.0;
+        double y = quickMul(x, N / 2);
+
+        if (N % 2 == 0)
+            return y*y;
+        else
+            return y*y*x;
+    }
+
+    /**
+     * 采用递归的快速幂算法
+     */
+    public double quickMul_myPow(double x, int n) {
+        long N = n;
+        if (N >= 0)
+            return quickMul(x, N);
+        else
+            return 1.0 / quickMul(x, -N);
     }
 
     public static void main(String[] args) {
