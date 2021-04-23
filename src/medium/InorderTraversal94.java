c36a3a14286ab766917924c8541de9a0cd7826ab
HEATAO <heatao59@gmail.com>
2021-04-23 19:26:08 +0800

    94没做完

---
+++ src/medium/InorderTraversal94.java
@@ -0,0 +1,16 @@
+package medium;
+
+import test.TreeNode;
+
+import java.util.List;
+
+/**
+ * 先序：考察到一个节点后，即刻输出该节点的值，并继续遍历其左右子树。(根左右)
+ * 中序：考察到一个节点后，将其暂存，遍历完左子树后，再输出该节点的值，然后遍历右子树。(左根右)
+ * 后序：考察到一个节点后，将其暂存，遍历完左右子树后，再输出该节点的值。(左右根)
+ */
+public class InorderTraversal94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        //
+    }
+}
