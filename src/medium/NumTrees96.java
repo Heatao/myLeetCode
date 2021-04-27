7edd9d21fd48e7f74f8a09fa1b7f96505a1143d8
HEATAO <heatao59@gmail.com>
2021-04-27 14:46:41 +0800

    96，DP

--- src/medium/NumTrees96.java
+++ src/medium/NumTrees96.java
@@ -4,9 +4,10 @@ package medium;
  * 心路历程：我也知道要用DP啊，但是状态转义方程式是啥呢～
  * 公式看这个吧：https://leetcode-cn.com/problems/unique-binary-search-trees/solution/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
  *
+ * 假设 n 个节点存在二叉排序树的个数是 G (n)，令 f(i) 为以 i 为根的二叉搜索树的个数，则
  * G(n)=f(1)+f(2)+f(3)+f(4)+...+f(n)
  * 当 i 为根节点时，其左子树节点个数为 i-1 个，右子树节点为 n-i，则
- * f(i) = G(i-1)*G(n-i)f(i)=G(i−1)∗G(n−i)
+ * f(i) = G(i-1)*G(n-i)
  * G(n)=G(0)∗G(n−1)+G(1)∗G(n−2)+...+G(n−1)∗G(0)
  */
 public class NumTrees96 {
