990f661449586b47d8902bddd50a928438cf709d
HEATAO <heatao59@gmail.com>
2021-07-20 14:08:44 +0800

    0720

--- src/test/Backpack.java
+++ src/test/Backpack.java
@@ -29,7 +29,26 @@ public class Backpack {
         return maxValue;
     }
 
-    public static void main(String[] args) {
+    // 注意这道题和518的区别！！！
+    private int backpackFull(int N, int V, int[] weights, int[] values){
+        if(V == 0 || weights.length == 0) return 0;
+        int[][] dp = new int[N+1][V+1];
+        // dp[i][j]表示前i个硬币重量为j时最大的价值
+        for(int i = 0; i <= N; i++)
+            dp[i][0] = 0;
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= V; j++) {
+                dp[i][j] = dp[i-1][j];
+                if(j - weights[i-1] >= 0) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-weights[i-1]] + values[i-1]);
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j-weights[i-1]] + values[i-1]);
+                }
+            }
+        }
+        return dp[N][V];
+    }
+
+    public static void main01() {
         Scanner in = new Scanner(System.in);
         int V = in.nextInt();
         int n = in.nextInt();
@@ -43,5 +62,23 @@ public class Backpack {
         Backpack backpack = new Backpack();
         int maxV = backpack.backPack01(values, weights, V, n);
         System.out.println(maxV);
+    }
+
+    public static void mainFull() {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int V = in.nextInt();                   // V是体积哟
+        int[] weights = new int[N];
+        int[] values = new int[N];
+        for(int i = 0; i < N; i++){
+            weights[i] = in.nextInt();
+            values[i] = in.nextInt();
+        }
+        Backpack solu = new Backpack();
+        System.out.println(solu.backpackFull(N, V, weights, values));
+    }
+
+    public static void main(String[] args) {
+        mainFull();
     }
 }
